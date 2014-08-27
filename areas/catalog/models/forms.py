74e761c108bc74cb0e8ffd240f50d24c0eff57dd
Mirco Macrelli <mmacrelli@gmail.com>
2014-08-27 15:51:51 +0200

    add an ISBN-10/13 validator to /catalog/add

--- areas/catalog/models/forms.py
+++ areas/catalog/models/forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import Form
 from wtforms import HiddenField, RadioField, StringField
 from wtforms.validators import DataRequired
+from isbn import valid_isbn
+
 
 __author__ = 'carlozamagni'
 
 
 class NewBookForm(Form):
-    isbn = StringField('isbn', validators=[DataRequired()], description={'placeholder': 'isbn'})
+    isbn = StringField('isbn', validators=[DataRequired(), valid_isbn], description={'placeholder': 'isbn'})
     title = StringField('title', validators=[DataRequired()], description={'placeholder': 'titolo'})
     author = StringField('author', validators=[DataRequired()], description={'placeholder': 'autore'})
     notes = StringField('notes', validators=[DataRequired()], description={'placeholder': 'note aggiuntive'})
